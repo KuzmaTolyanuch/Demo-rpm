c0d213d16811a8055552d70432eb960efef9183d
Valerii Kuzmenko <vaken111@gmail.com>
2018-07-19 15:30:00 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Disable password authentication
 sudo grep -q "^[^#]*PasswordAuthentication" /etc/ssh/sshd_config && sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config || echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-sudo service ssh restart
+sudo service sshd restart
